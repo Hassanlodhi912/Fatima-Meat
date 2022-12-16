a8c615686ed51c1d2aed724d9940b726494dacd8
hassan <muhammadhassanlodhi123@gmail.com>
2022-12-16 17:01:25 +0500

    project discriptio nui added

--- App.js
+++ App.js
@@ -2,14 +2,17 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet} from 'react-native';
 import HomeScreen from './src/pages/HomeScreen';
 import LoginScreen from './src/pages/LoginScreen';
+import Productpage from './src/pages/DetailsScreen';
 import SignUpScreen from './src/pages/SignUpScreen';
 import Welcome from './src/pages/Welcome';
+import DetailsScreen from './src/pages/DetailsScreen';
 export default function App() {
   return( 
     // <Welcome/>
-    <HomeScreen/>
+    // <HomeScreen/>
     // <SignUpScreen/>
     // <LoginScreen/>
+    <DetailsScreen/>
     )
   }
   
